36bc0ae960c87333d7795912965f8205660c44c5
jonathanlang <accounts@jonathanlang.de>
2014-03-23 19:17:53 +0100

    Create categories.php

---
+++ de_DE/categories.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+
+	'category' => 'Kategorie',
+	'categories' => 'Kategorien',
+
+	'create_category' => 'Erstelle eine neue Kategorie',
+	'edit_category' => 'Bearbeite &ldquo;%s&rdquo;',
+
+	// form fields
+	'title' => 'Titel',
+	'title_explain' => 'Dein Kategorie-Titel.',
+	'title_missing' => 'Hier fehlt ein Titel',
+
+	'slug' => 'Slug',
+	'slug_explain' => 'Die URL für deine Kategorie.',
+
+	'description' => 'Beschreibung',
+	'description_explain' => 'Worum es in der Kategorie geht.',
+
+	// messages
+	'created' => 'Deine neue Kategorie wurde hinzugefügt.',
+	'updated' => 'Deine Kategorie wurde aktualisiert.',
+	'deleted' => 'Deine Kategorie wurde gelöscht.',
+	'delete_error' => 'Mindestens eine Kategorie benötigt.',
+
+);
