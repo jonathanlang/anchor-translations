e1b62369a4f10e3b122c8243878b249a0c7caf38
jonathanlang <accounts@jonathanlang.de>
2014-03-23 19:13:17 +0100

    Create global.php
    Erste Übersetzung für global.php

---
+++ de_DE/global.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * Global is to replace common.php
+ *
+ * This global file will provide the majority of the translations in anchor
+ * to remove duplicate translations in section specific language files.
+ */
+return array(
+
+	// words
+	'save' => 'Speichern',
+	'delete' => 'Löschen',
+	'update' => 'Aktualisieren',
+	'edit' => 'Bearbeiten',
+	'editing' => 'Bearbeitung',
+	'create' => 'Erstellen',
+	'created' => 'Erstellt',
+	'submit' => 'Fertig',
+	'close' => 'Schließen',
+	'status' => 'Status',
+	'manage' => 'Verwalten',
+	'reset' => 'Reset',
+	'all' => 'Alle',
+
+	// pagination
+	'next' => 'Nächste',
+	'previous' => 'Vorherige',
+	'first' => 'Erste',
+	'last' => 'Letzte',
+
+	// statuses
+	'draft' => 'Entwurf',
+	'archived' => 'Archiviert',
+	'published' => 'Veröffentlicht',
+	'pending' => 'Wartend',
+	'approved' => 'Bestätigt',
+	'spam' => 'Spam',
+
+	'inactive' => 'Inaktiv',
+	'active' => 'Aktiv',
+
+	// roles
+	'administrator' => 'Admin',
+	'editor' => 'Editor',
+	'user' => 'User',
+
+	'log_in' => 'Anmelden',
+	'login' => 'Login',
+	'log_out' => 'Abmelden',
+	'logout' => 'Logout',
+
+	// pharses
+	'visit_your_site' => 'Seite besuchen',
+	'powered_by_anchor' => 'Powered by Anchor, version %s',
+	'make_blogging_beautiful' => 'Make blogging beautiful.',
+
+	// intro
+	'welcome_to_anchor' => 'Willkommen zu Anchor',
+	'welcome_to_anchor_lets_go' => 'Willkommen zu Anchor. Lass uns loslegen!',
+	'run_the_installer' => 'Installer ausführen',
+
+	// upgrade
+	'upgrade' => 'Upgrade',
+	'good_news' => 'Tolle Neuigkeit!',
+	'new_version_available' => 'Es gibt eine neue Version von Anchor.',
+	'download_now' => 'Download',
+	'upgrade_later' => 'Später',
+
+	// debug profiler
+	'profile' => 'Profile',
+	'profile_memory_usage' => 'Gesamter Speicherbedarf',
+
+	// messages
+	'confirm_delete' => 'Bist du sicher dass du das löschen willst? Dies kann nicht rückgängig gemacht werden!'
+
+);
