d4e0043f85fe19ebac2bd6bbef09ec5a7d95f38d
jonathanlang <accounts@jonathanlang.de>
2014-03-23 19:20:45 +0100

    Create plugins.php

---
+++ de_DE/plugins.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+
+	'plugins' => 'Plug-Ins',
+	'plugins_desc' => 'Verwalte deine Plug-Ins',
+	'noplugins_desc' => 'Tob dich im Code aus',
+
+	// messages
+	'installed' => '%s wurde installiert',
+	'uninstalled' => '%s wurde entfernt'
+);
